4d2ed431fd71474d81b7dfa78736c698e5460af9
txb13 <tbeneke46@gmail.com>
2025-10-30 13:32:27 +0100

    getUserNumber() implemented

--- javasrc/de/htwberlin/dbtech/aufgaben/ue02/MautVerwaltungImpl.java
+++ javasrc/de/htwberlin/dbtech/aufgaben/ue02/MautVerwaltungImpl.java
@@ -1,6 +1,9 @@
 package de.htwberlin.dbtech.aufgaben.ue02;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +24,8 @@ public class MautVerwaltungImpl implements IMautVerwaltung {
 		this.connection = connection;
 	}
 
+
+
 	private Connection getConnection() {
 		if (connection == null) {
 			throw new DataException("Connection not set");
@@ -31,13 +36,51 @@ public class MautVerwaltungImpl implements IMautVerwaltung {
 	@Override
 	public String getStatusForOnBoardUnit(long fzg_id) {
 		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement preparedstatement = null;
+		ResultSet resultSet = null;
+		String query = "SELECT STATUS FROM FAHRZEUGGERAT WHERE FZG_ID = ?";
+		String res = "";
+		try {
+
+			preparedstatement = getConnection().prepareStatement(query);
+			preparedstatement.setLong(1, fzg_id);
+			resultSet = preparedstatement.executeQuery();
+			if (resultSet.next()) {
+				res = resultSet.getString("STATUS");
+			}
+		} catch (SQLException exp) {
+			throw new RuntimeException(exp);
+		} catch (NullPointerException exp) {
+			throw new RuntimeException(exp);
+		}
+		return res;
 	}
 
 	@Override
 	public int getUsernumber(int maut_id) {
-		// TODO Auto-generated method stub
-		return 0;
+		int nutzer_id = 0;
+
+		String sql = "SELECT FZ.NUTZER_ID " +
+				"FROM MAUTERHEBUNG ME " +
+				"INNER JOIN FAHRZEUGGERAT FZG ON ME.FZG_ID = FZG.FZG_ID " +
+				"INNER JOIN FAHRZEUG FZ ON FZG.FZ_ID = FZ.FZ_ID " +
+				"WHERE ME.MAUT_ID = ?";
+
+		try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+
+			preparedStatement.setInt(1, maut_id);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+				if (resultSet.next()) {
+					nutzer_id = resultSet.getInt("NUTZER_ID");
+				}
+			}
+		} catch (SQLException e) {
+			System.err.println("SQL-Fehler beim Abrufen der Nutzernummer: " + e.getMessage());
+		}
+		return nutzer_id;
 	}
 
 	@Override

316c185058dd190ef8be8d84926662f6fcec2294
txb13 <tbeneke46@gmail.com>
2025-11-12 15:08:00 +0100

    delete DbemptyCred

--- javasrc/de/htwberlin/dbtech/utils/DbCredEmpty.java
+++
@@ -1,9 +0,0 @@
-package de.htwberlin.dbtech.utils;
-
-public interface DbCredEmpty {
-    String driverClass = "oracle.jdbc.driver.OracleDriver";
-    String url = "jdbc:oracle:thin:@aaa:1521:orcl";
-    String user = "uuu";
-    String password = "ppp";
-    String schema = "uuu";
-}
